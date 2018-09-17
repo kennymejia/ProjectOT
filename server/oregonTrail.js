ea898b551f8edc1404f9eeaffd777de1f64742be
kennymejia <kenny.mejia1@marist.edu>
2018-09-16 21:17:04 -0400

    part 4 step 1 completed (index)

--- server/oregonTrail.js
+++ server/oregonTrail.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 1337
 
-app.get('/', function (req, res) 
-{ 
-	res.sendFile( 'index.html', {root: './client/views'} )
-})
+app.get('/', function (req, res) { 
+	res.sendFile('index.html', {root: './client/views'}) })
+app.get('/mainmenu', function (req, res) { 
+	res.sendFile('mainmenu.html', {root: './client/views'}) })
+app.get('/setup', function (req, res) { 
+	res.sendFile('setup.html', {root: './client/views'}) })
+app.get('/trail', function (req, res) { 
+	res.sendFile('trail.html', {root: './client/views'}) })
+app.get('/topten', function (req, res) { 
+	res.sendFile('topten.html', {root: './client/views'}) })
 
 app.use(express.static('client/public'));
 
