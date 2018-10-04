48352a7b423ae4b9e15b4fa19d0962df40bd4b43
kennymejia <kenny.mejia1@marist.edu>
2018-10-04 19:28:53 -0400

    Client Side Of Project Complete

---
+++ client/public/js/trail.js
@@ -0,0 +1,26 @@
+//FUNCTION FOR THE FADE ON TEXT REFERENCING QUOTES			  
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
+
+//'LISTENS' FOR THE ESCAPE KEY
+document.body.addEventListener("keydown", function (event) {
+   
+    if (event.keyCode === 16) {
+        window.location.replace("http://localhost:1337/mainmenu.html");
+    }
+    
+});
